4318c5d40a6a38c4be8e521f273b00c145cb3c36
BoikoIgor <111757412+BoikoIgor@users.noreply.github.com>
2023-04-25 23:42:58 +0000

    Deploying to gh-pages from @ BoikoIgor/parcel-JS-tests@982501b0dea0fc863df4f4f01b9cbbba841f75d4 🚀

---
+++ index.6aa3902f.js
@@ -0,0 +1,2 @@
+!function(){var n,o,r=3;console.log(((o=(n=["11","22","33","44","55"]).map((function(n,o){return o}))).sort((function(n,o){return o-n})),o.slice(0,r).map((function(o){return n[o]}))))}();
+//# sourceMappingURL=index.6aa3902f.js.map
---
+++ index.ff3b003c.js
@@ -0,0 +1,2 @@
+let e=3;console.log(function(l,o){let t=l.map(((e,l)=>l));return t.sort(((e,l)=>l-e)),t.slice(0,e).map((e=>l[e]))}(["11","22","33","44","55"]));
+//# sourceMappingURL=index.ff3b003c.js.map
